a2ba4d807a3fd720a24118164b6785486aa445b6
juacompe <juacompe@gmail.com>
2016-08-17 15:08:13 +0700

    keyword package fails since roborframeowrk version 3

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'robotframework-serverspeclibrary',
-    version = '0.1',
+    version = '0.2',
     description = 'Server spec on Robot Framework inspired from Serverspec on Ruby',
     url = 'https://github.com/wingyplus/robotframework-serverspeclibrary',
     keywords = 'serverspec robotframework robot',
     license = 'MIT',
     author = 'Thanabodee Charoenpiriyakij',
     author_email = 'wingyminus@gmail.com',
-    install_requires = ['robotframework>=2.8.5', 'paramiko>=1.14.0'],
+    install_requires = ['robotframework>=2.8.5,<3', 'paramiko>=1.14.0'],
     package_dir = {'': 'src'},
     packages = ['ServerSpecLibrary', 'ServerSpecLibrary.keywords']
 )
